90b977d3f5ecc7ccb10069a4f62fc5e92b59eb62
rasdarps <rasdarps@gmail.com>
2023-05-12 05:48:46 +0000

    new up

--- login.php
+++ login.php
@@ -8,17 +8,73 @@
 
     <title>Document</title>
 
+    <style>
+        body{
+        background-image:url('ceo_jobs.jpg');
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-attachment: fixed;
+        background-size: cover;
+    }
+
+    .container-fluid{
+        background: transparent;
+        backdrop-filter: blur(25px);
+    }
+    </style>
+
 </head>
 <body>
 
-    <div class="container">
-        <div class="card">
-            <form action="">
-                <div>
-                    <input type="text">
+    <div class="container-fluid h-100 p-5">
+        <div class="col-md-8 mx-auto">
+
+            <div class="card col-md-6 mx-auto login p-5 my-3">
+                <div class="logo-img text-center">
+                <img src="logo.png" alt="" width="80">   
                 </div>
-            </form>
+                <h3 class="text-center">Login Form</h3>
+                <form action="" method="post" class="">
+                    <div class="form-group m-3">
+                        <label for="username">Username</label>
+                        <div class="input-group">
+                        <input type="text" name="username" class="form-control">
+                        </div>
+                        
+                    </div>
+                    <div class="form-group m-3">
+                        <label for="password">Password</label>
+                        <div class="input-group">
+                        <input type="text" name="password" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="row m-3">
+                        <div class="col-md-6">
+                            <input type="checkbox" name="remember_me"> Remember Me
+                        </div>
+                        
+
+                    </div>
+
+
+                    <div class="text-center my-2">
+                        <button type="submit" class="btn btn-success btn-block">Submit</button>
+                    </div>
+
+                    <div class="text-center">
+                            <span>Don't have an account? </span><a href="#" style="text-decoration:none;">Register</a>
+                        </div>
+                </form>
+            </div>
+
         </div>
+
+        <footer>
+            <div class="text-center">
+                <p>2018 - <?php echo date('Y')?> (c) <a href="#"> RICT </a> </p>
+            </div>
+        </footer>
     </div>
 
 
